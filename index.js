3f1af90703a1eefd8d8ed56416ea5b5f56a63958
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-03-20 20:37:50 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(array){
+    let wn =item=>item.result === 'W';
+    let win = array.find(wn);
+    /*if(win){
+        return win.year
+    }
+    return win*/
+    return win?win.year:win;
+}
